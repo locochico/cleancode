04c234f9cbf62d01b4bfe500570e2d0692f3ce06
201210105 <201210105@SKMNS-201210105.skmns.co.kr>
2016-08-23 11:10:33 +0900

    0. Add Characterization Test

--- cleancode/src/test/java/print/primenumber/function/PrintPrimesTest.java
+++ cleancode/src/test/java/print/primenumber/function/PrintPrimesTest.java
@@ -16,8 +16,29 @@ import java.io.PrintStream;
 import static org.junit.Assert.assertEquals;
 
 public class PrintPrimesTest {
-	@Test
-	public void should_print_result() {
-		PrintPrimes.main(new String[0]);
-	}
+    private PrintStream out;
+
+    @Before
+    public void setUp() throws FileNotFoundException {
+        out = System.out;
+        System.setOut(new PrintStream(new FileOutputStream("lead")));
+    }
+
+   @After
+    public void tearDown() {
+        System.setOut(out);
+        new File("lead").delete();
+    }
+
+    @Test
+    public void
+    makeSureMatchesGold() throws IOException {
+        new PrintPrimes().main(new String[0]);
+        BufferedReader lead = new BufferedReader(new FileReader("lead"));
+        BufferedReader gold = new BufferedReader(new FileReader("src/test/java/print/primenumber/function/gold"));
+        String line;
+        while((line = gold.readLine()) != null)
+            assertEquals(line, lead.readLine());
+      //  assertEquals(null, lead.readLine());
+    }
 }
