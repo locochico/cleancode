2abb8618a72fbe0a055eb6304204c1f7975184f0
201210105 <201210105@SKMNS-201210105.skmns.co.kr>
2016-08-23 18:09:22 +0900

    9. prepare extract method object
        linesPerPage, columns는 printNumbers에서만 사용된다.
        이 필드들은 후에 printNumbers가 별도의 클래스로 추출되면 PrimePrinterHelper에 있을 필요가 없다.
        이 필드들을 PrintPrimes로 이동시킨다.
    
    10. extract method object - NumberPrinter

--- cleancode/src/main/java/print/primenumber/function/PrintPrimes.java
+++ cleancode/src/main/java/print/primenumber/function/PrintPrimes.java
@@ -55,20 +55,6 @@ class PrimePrinterHelper {
 	}
 	
 	public void printNumbers(int[] numbers, int numberOfPrimes, int linesPerPage, int columns) {
-		int pagenumber = 1;
-		int pageoffset = 1;
-		while (pageoffset <= numberOfPrimes) {
-			System.out.println("The First " + numberOfPrimes + " Prime Numbers --- Page " + pagenumber);
-			System.out.println("");
-			for (int rowoffset = pageoffset; rowoffset < pageoffset + linesPerPage; rowoffset++) {
-				for (int column = 0; column < columns; column++)
-					if (rowoffset + column * linesPerPage <= numberOfPrimes)
-						System.out.format("%10d", numbers[rowoffset + column * linesPerPage]);
-				System.out.println("");
-			}
-			System.out.println("\f");
-			pagenumber = pagenumber + 1;
-			pageoffset = pageoffset + linesPerPage * columns;
-		}
+		new NumberPrinter(numbers, numberOfPrimes, linesPerPage, columns).invoke();
 	}
 }
